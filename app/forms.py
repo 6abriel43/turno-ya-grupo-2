c6bc48446f9841a84b35712e0efabf2254915dc4
6abriel43 <gabrielguitian202@gmail.com>
2026-05-24 18:43:58 -0300

    feat: agrego metodo clean() + validaciones a formulario Turno

--- app/forms.py
+++ app/forms.py
@@ -12,3 +12,17 @@ class TurnoForm(forms.ModelForm):
             'paciente': forms.Select(attrs={'class': 'form-select'}),
             'motivo': forms.TextInput(attrs={'class': 'form-control'}),
         }
+    def clean(self):
+        cleaned_data = super().clean()
+        fecha = cleaned_data.get('fecha_hora')
+        medico = cleaned_data.get('medico')
+        
+        #Validacion 1: Fecha pasada
+        if fecha and fecha < timezone.now():
+            raise forms.ValidationError("No se pueden solicitar turnos en fechas pasadas.")
+        #Validación 2: Médico ocupado (Validación personalizada)
+        if fecha and medico:
+            if Turno.objects.filter(medico=medico, fecha_hora=fecha, estado="ACEPTADO").exists():
+                raise forms.ValidationError(f"El Dr./a {medico.apellido} ya tiene un turno aceptado en este horario.")
+        
+        return cleaned_data
