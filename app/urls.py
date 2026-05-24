af6effa2eb5ff6c1d5030075567aaa10a3a360e7
6abriel43 <gabrielguitian202@gmail.com>
2026-05-24 18:59:44 -0300

    feat: agrego la ruta de TurnoCreate en urlpatterns urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@
 
 from django.urls import path
 from . import views
+from .views import TurnoCreateView
 
 app_name = "app"
 
@@ -9,8 +10,8 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("medicos/", views.ListaMedicosView.as_view(), name="lista_medicos"),
     # TODO:
+    path('turno/nuevo/', TurnoCreateView.as_view(), name='crear_turno'),
     # path("medicos/<int:pk>/", views.DetalleMedicoView.as_view(), name="detalle_medico"),
     # path("turnos/", views.ListaTurnosView.as_view(), name="lista_turnos"),
-    # path("turnos/nuevo/", views.NuevoTurnoView.as_view(), name="nuevo_turno"),
     # path("turnos/<int:pk>/cancelar/", views.CancelarTurnoView.as_view(), name="cancelar_turno"),
 ]
