d0a5beac713f050444b6713bece0eec1f2ffa83b
Facundo Zamora <zamora.facundo2@gmail.com>
2026-05-31 13:06:43 -0300

    feat: agregar rutas para detalle de médico y lista de pacientes

--- app/urls.py
+++ app/urls.py
@@ -2,19 +2,22 @@
 
 from django.urls import path
 from . import views
-from .views import TurnoCreateView
+from .views import TurnoCreateView, MedicoDetailView, ListaPacientesView
 
 app_name = "app"
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("medicos/", views.ListaMedicosView.as_view(), name="lista_medicos"),
+    path("medicos/<int:pk>/", views.DetalleMedicoView.as_view(), name="detalle_medico"),
+    path("pacientes/", ListaPacientesView.as_view(), name="lista_pacientes"),
+
     # TODO:
     path('turno/nuevo/', TurnoCreateView.as_view(), name='crear_turno'),
 
     path('ausencias/', views.AusenciaListView.as_view(), name='lista_ausencias'),
     path('recordatorios/', views.RecordatorioListView.as_view(), name='lista_recordatorios'),
-    # path("medicos/<int:pk>/", views.DetalleMedicoView.as_view(), name="detalle_medico"),
+
     # path("turnos/", views.ListaTurnosView.as_view(), name="lista_turnos"),
     # path("turnos/<int:pk>/cancelar/", views.CancelarTurnoView.as_view(), name="cancelar_turno"),
 ]
