6e01c787a77fa47f92fa031df1757c72d2ab1983
6abriel43 <gabrielguitian202@gmail.com>
2026-06-01 02:30:50 -0300

    feat: agrego al urlpatterns ruta de ListaTurnosView

--- app/urls.py
+++ app/urls.py
@@ -13,13 +13,12 @@ urlpatterns = [
     path("pacientes/", ListaPacientesView.as_view(), name="lista_pacientes"),
 
     # TODO:
-    path('turno/nuevo/', TurnoCreateView.as_view(), name='crear_turno'),
-
+    path('turnos/nuevo/', views.TurnoCreateView.as_view(), name='crear_turno'),
+    path('turnos/', views.ListaTurnosView.as_view(), name='lista_turnos'),
+    path('turnos/<int:pk>/cancelar/', views.CancelarTurnoView.as_view(), name='cancelar_turno'),
     path('ausencias/', views.AusenciaListView.as_view(), name='lista_ausencias'),
     path('recordatorios/', views.RecordatorioListView.as_view(), name='lista_recordatorios'),
 
     # path("turnos/", views.ListaTurnosView.as_view(), name="lista_turnos"),
     # path("turnos/<int:pk>/cancelar/", views.CancelarTurnoView.as_view(), name="cancelar_turno"),
 ]
-
-   
